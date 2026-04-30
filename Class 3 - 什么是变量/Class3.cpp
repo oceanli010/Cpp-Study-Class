94cbe366f7e82970453cad9a0932d833620cca38
ocean_li <oceanli010@163.com>
2026-04-30 23:13:57 +0800

    第一次更新

---
+++ Class 3 - 什么是变量/Class3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    int a = 10;
+    float b = 3.14;
+    double c = 3.14;
+    char d = 'A';
+    long long e = 202604300000;
+
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << c << std::endl;
+    std::cout << d << std::endl;
+    std::cout << e << std::endl;
+
+    return 0;
+}
